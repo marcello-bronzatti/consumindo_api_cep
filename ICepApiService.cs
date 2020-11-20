3880bfd19810f9838aa271bedc72b1c756fae3f7
Marcello Bronzatti <mbsimogaki@gmail.com>
2020-11-20 09:53:09 -0300

    Consumindo API REST do site via cep.

---
+++ ICepApiService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace ExemploRefit
+{
+    public interface ICepApiService
+    {
+        [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Refit;
+using System;
+using System.Threading.Tasks;
+
+namespace ExemploRefit
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                var cepClient = RestService.For<ICepApiService>("http://viacep.com.br");
+                Console.WriteLine("Informe seu cep: ");
+
+                string cepInformado = Console.ReadLine().ToString();
+                Console.WriteLine("Consultado informaçoes do CEP: "  + cepInformado);
+
+                var addres = await cepClient.GetAddressAsync(cepInformado);
+
+                Console.Write($"\nLogradouro: {addres.Logradouro},\nBairro: {addres.Bairro},\nCidade: {addres.Localidade}");
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta de CEP" + e.Message);
+            }
+        }
+    }
+}
